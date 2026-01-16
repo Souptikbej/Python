0fb6dcf665bd93a6ab9117fc314f780ff1925a52
Souptikbej <bejsouptik@gmail.com>
2026-01-16 22:03:51 +0530

    day1

---
+++ week1-2/week1/Day1.py
@@ -0,0 +1,22 @@
+#Write a program to display a person's name, age and address in three different lines
+print("Name: Souptik Bej")
+print("Age: 23")
+print("Address: Singur, Burashanti, Hooghly, West Bengal")
+
+#Write a program to swap two variables.
+
+
+#Write a program to convert a float into integer.
+a=20.36
+b=int(a)
+print(b)
+
+#Write a program to take details from a student for ID-card and then print it in different lines.
+print("Give Details For Making ID-Card")
+name=input("Enter Your Name: ")
+age=int(input("Enter Your Age: "))
+dept=input("Enter Your Depertment: ")
+
+#Write a program to taka an user input as integer the convert to float.
+num1=float(input("Enter a integer number: "))
+print(num1)
